9e80ed0d44343f96a2a8099639de0093f2f46228
C#Developer <shahzodkiller11@gmail.com>
2020-03-29 01:38:06 +0500

    Mine

--- Program.cs
+++ Program.cs
@@ -6,114 +6,24 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            ///////////////////////2
-            int a = 1, b = 3, sum = 0, sum2 = 0;
-            for (int i = a + 1; i < b; i++)
+            int tries = 10;
+            string rightanswer="iphone";//Правильный ответ
+            System.Console.WriteLine("Letaet Bobrov kusaet");
+            for (int i = 0; i <= tries; tries--)
             {
-                sum += i;
-                if (i % 2 == 1)
+                System.Console.WriteLine("Answer:");
+                string answer = Console.ReadLine();
+                if(answer.ToLower()==rightanswer.ToLower())
                 {
-                    sum2 += i;
+                    System.Console.WriteLine("Thats right answer Congratulations!!!!!!!!!!!!!!!!!");
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("Thats Incorrect answer");
+                    System.Console.WriteLine($"You have {tries} tries");
                 }
             }
-            System.Console.WriteLine(sum);
-            System.Console.WriteLine(sum2);
-
-            ////////////////////////////////3
-            //pryamougolnik
-            for (int i = 0; i < 30; i++) { System.Console.Write("*"); }
-            for (int i = 0; i <= 5; i++) { System.Console.WriteLine("*                             *"); }
-            for (int i = 0; i <= 30; i++) { System.Console.Write("*"); }
-            System.Console.WriteLine("");
-            System.Console.WriteLine("");
-
-
-            //treugolnik
-            int count = 0;
-            for (int i = 0; i <= 10; i++)
-            {
-                System.Console.Write("*");
-                for (int y = 0; y <= count; y++)
-                {
-                    Console.Write(" ");
-                }
-                System.Console.Write("*");
-                count += 2;
-                System.Console.WriteLine();
-            }
-            for (int i = 0; i <= 23; i++)
-            {
-                System.Console.Write("*");
-            }
-
-
-            //treugolnik(ravnostoronniy)
-            int tr = 10;
-            int tr2 = 0;
-            for(int y=0;y<=10;y++)
-            {
-            for (int i = 0; i <= tr; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.Write("*");
-            for(int i = 0; i < tr2; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.WriteLine("*");
-            tr--;
-            tr2+=2;
-            }
-            for(int i=0;i<=23;i++)System.Console.Write("*");
-
-            //Romb
-            int tr3 = 10;
-            int tr4 = 0;
-            for(int y=0;y<=5;y++)
-            {
-            for (int i = 0; i <= tr3; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.Write("*");
-            for(int i = 0; i < tr4; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.WriteLine("*");
-            tr3-=2;
-            tr4+=4;
-            }
-            tr3=0;
-            tr4=20;
-            for(int y=0;y<=5;y++)
-            {
-            for (int i = 0; i <= tr3; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.Write("*");
-            for(int i = 0; i < tr4; i++)
-            {
-                System.Console.Write(" ");
-            }
-            System.Console.WriteLine("*");
-            tr3+=2;
-            tr4-=4;
-            }
-
-            ///////////////////////////////////4
-            double money = 1000;
-            double procent = 1;
-            int month = 0;
-            while (money <= 1100)
-            {
-                money += money * procent / 100;
-                month++;
-            }
-            System.Console.WriteLine("month:" + month);
-            System.Console.WriteLine("money:" + money);
         }
     }
 }
