c7d0f963ae292e6aee74da6526dd68d6cc682490
C#Developer <shahzodkiller11@gmail.com>
2020-04-01 13:54:04 +0500

    First

--- Program.cs
+++ Program.cs
@@ -6,24 +6,100 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int tries = 10;
-            string rightanswer="iphone";//Правильный ответ
-            System.Console.WriteLine("Letaet Bobrov kusaet");
-            for (int i = 0; i <= tries; tries--)
-            {
-                System.Console.WriteLine("Answer:");
-                string answer = Console.ReadLine();
-                if(answer.ToLower()==rightanswer.ToLower())
-                {
-                    System.Console.WriteLine("Thats right answer Congratulations!!!!!!!!!!!!!!!!!");
-                    break;
-                }
-                else
-                {
-                    System.Console.WriteLine("Thats Incorrect answer");
-                    System.Console.WriteLine($"You have {tries} tries");
-                }
-            }
+            //////////////////////////////////////////////////////////////////////////////////////2
+            //  System.Console.Write("Write The Length of array:");
+            // int num = int.Parse(Console.ReadLine()); int max = 0; int min = 0; int sum = 0, numeric = 0;
+            // int[] array = new int[num];
+            // for (int i = 0; i < num; i++)
+            // {
+            //     int Random = new Random().Next(1, 999);
+            //     array[i] = Random;
+            //     if (array[i] % 2 == 1)
+            //     {
+            //         numeric++;
+            //     }
+            // }
+            // int[] array2 = new int[numeric];
+            // for (int i = 0; i < array.Length-1; i++)
+            // {
+            //     if (array[i] % 2 == 1)
+            //     {
+            //         array2[i] = array[i];
+            //     }
+            // }
+            // min = array[0];
+            // max = array[0];
+            // foreach (var item in array)
+            // {
+            //     if (max < item)
+            //     {
+            //         max = item;
+            //     }
+            //     if (min > item)
+            //     {
+            //         min = item;
+            //     }
+            //     sum += item;
+            // }
+            // System.Console.WriteLine($"max:{max}");
+            // System.Console.WriteLine($"min:{min}");
+            // System.Console.WriteLine($"sum:{sum}");
+            // System.Console.WriteLine($"Srednee Arifmeticheskoe:{sum / array.Length}");
+            // System.Console.WriteLine("Nechotnie:");
+            // foreach (var item in array2)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+            ////////////////////////////////////////////////////////////////////////////////////3
+            // int[] nums = { 1, 2, 3, 4, 5 }; int counter = 0;
+            // int[] nums2 = new int[nums.Length];
+            // for (int i = nums.Length - 1; i >= 0; i--)
+            // {
+            //     nums2[counter] = nums[i];
+            //     counter++;
+            // }
+            // foreach (var item in nums2)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+
+            //////////////////////////////////////////////////////////////4
+            
+            // System.Console.Write("Write The Length of array:");
+            // int num = int.Parse(Console.ReadLine());
+            // int[] array = new int[num];
+            // for (int i = 0; i < num; i++)
+            // {
+            //     int Random = new Random().Next(1, 999);
+            //     array[i] = Random;
+            // }
+            // foreach (var item in array)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
+            // System.Console.Write("index:");
+            // int index = int.Parse(Console.ReadLine());
+            // System.Console.Write("count:");
+            // int count = int.Parse(Console.ReadLine());
+            // int[] array2 = new int[count];
+            // int count2 = 0;
+
+            // for (int i = index; i < count + index; i++)
+            // {
+            //     if (i <= array.Length-1)
+            //     {
+            //         array2[count2] = array[i];
+            //         count2++;
+            //     }
+            //     else
+            //     {
+            //         array2[count2] = 1;
+            //     }
+            // }
+            // foreach (var item in array2)
+            // {
+            //     System.Console.WriteLine(item);
+            // }
         }
     }
 }
