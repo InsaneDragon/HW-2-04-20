2fc0ac313689876d72b4a88327ab3da7cb4717f6
C#Developer <shahzodkiller11@gmail.com>
2020-04-02 22:50:41 +0500

    FirstTask(Completed)

--- Program.cs
+++ Program.cs
@@ -6,100 +6,25 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            //////////////////////////////////////////////////////////////////////////////////////2
-            //  System.Console.Write("Write The Length of array:");
-            // int num = int.Parse(Console.ReadLine()); int max = 0; int min = 0; int sum = 0, numeric = 0;
-            // int[] array = new int[num];
-            // for (int i = 0; i < num; i++)
-            // {
-            //     int Random = new Random().Next(1, 999);
-            //     array[i] = Random;
-            //     if (array[i] % 2 == 1)
-            //     {
-            //         numeric++;
-            //     }
-            // }
-            // int[] array2 = new int[numeric];
-            // for (int i = 0; i < array.Length-1; i++)
-            // {
-            //     if (array[i] % 2 == 1)
-            //     {
-            //         array2[i] = array[i];
-            //     }
-            // }
-            // min = array[0];
-            // max = array[0];
-            // foreach (var item in array)
-            // {
-            //     if (max < item)
-            //     {
-            //         max = item;
-            //     }
-            //     if (min > item)
-            //     {
-            //         min = item;
-            //     }
-            //     sum += item;
-            // }
-            // System.Console.WriteLine($"max:{max}");
-            // System.Console.WriteLine($"min:{min}");
-            // System.Console.WriteLine($"sum:{sum}");
-            // System.Console.WriteLine($"Srednee Arifmeticheskoe:{sum / array.Length}");
-            // System.Console.WriteLine("Nechotnie:");
-            // foreach (var item in array2)
-            // {
-            //     System.Console.WriteLine(item);
-            // }
-            ////////////////////////////////////////////////////////////////////////////////////3
-            // int[] nums = { 1, 2, 3, 4, 5 }; int counter = 0;
-            // int[] nums2 = new int[nums.Length];
-            // for (int i = nums.Length - 1; i >= 0; i--)
-            // {
-            //     nums2[counter] = nums[i];
-            //     counter++;
-            // }
-            // foreach (var item in nums2)
-            // {
-            //     System.Console.WriteLine(item);
-            // }
-
-            //////////////////////////////////////////////////////////////4
-            
-            // System.Console.Write("Write The Length of array:");
-            // int num = int.Parse(Console.ReadLine());
-            // int[] array = new int[num];
-            // for (int i = 0; i < num; i++)
-            // {
-            //     int Random = new Random().Next(1, 999);
-            //     array[i] = Random;
-            // }
-            // foreach (var item in array)
-            // {
-            //     System.Console.WriteLine(item);
-            // }
-            // System.Console.Write("index:");
-            // int index = int.Parse(Console.ReadLine());
-            // System.Console.Write("count:");
-            // int count = int.Parse(Console.ReadLine());
-            // int[] array2 = new int[count];
-            // int count2 = 0;
-
-            // for (int i = index; i < count + index; i++)
-            // {
-            //     if (i <= array.Length-1)
-            //     {
-            //         array2[count2] = array[i];
-            //         count2++;
-            //     }
-            //     else
-            //     {
-            //         array2[count2] = 1;
-            //     }
-            // }
-            // foreach (var item in array2)
-            // {
-            //     System.Console.WriteLine(item);
-            // }
+          Console.Write("Length:");double len=double.Parse(Console.ReadLine());
+          Console.Write("Width:");double width=double.Parse(Console.ReadLine());
+          Rectange MyRec=new Rectange(len,width);   
+          System.Console.WriteLine(MyRec.Area);
+          System.Console.WriteLine(MyRec.Perimetr);
         }
+        static double AreaCalculator(double a,double b)
+        {
+            return a*b;
+        }
+        static double PerimetrCalculator(double a,double b)
+        {
+            return 2*(a+b);
+        }
+    }
+    class Rectange
+    {
+        public double side1 { get; set; }public double side2 { get; set; }public Rectange(double a,double b){side1=a;side2=b;}
+        public double Area{get{return side1*side2;}}public double Perimetr{get{return 2*(side1+side2);}}
+        
     }
 }
